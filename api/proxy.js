506727d79c635943b8826a0ec40247c79f3410af
debumich <157843161+debumich@users.noreply.github.com>
2026-09-04 20:19:57 +0300

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,6 +1,15 @@
 export default async function handler(req, res) {
   const targetUrl = 'https://script.google.com/macros/s/AKfycbxyybXqtVcpuKnmbbgXG_fRldZ3cO1GiIZtt_G8P3VENIdyoT3_jHNRbyokfjfvtzBn/exec';
   
+  // Обработка preflight (OPTIONS) запроса
+  if (req.method === 'OPTIONS') {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Cache-Control, Pragma, Expires');
+    res.status(200).end();
+    return;
+  }
+
   try {
     const response = await fetch(targetUrl + '?' + new URLSearchParams(req.query).toString(), {
       headers: {
